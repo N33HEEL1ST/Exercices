df5ecbb0d1116fb3c58e7d372a6caee4bda0ed90
N33HEEL1ST <mr.johnnylima@gmail.com>
2017-08-08 12:38:47 +0200

    CHANGE -- home.php

--- EXERCICES/Projet_Micromonio/app/Views/default/home.php
+++ EXERCICES/Projet_Micromonio/app/Views/default/home.php
@@ -2,9 +2,7 @@
 
 <?php $this->start('main_content') ?>
 	<h2>Project Micromonio</h2>
-	<ul>
-		<li><a href="#">list with link</a></li>
-	</ul>
-	<p>paragraph</p>
+
+	<p>This home is lame and there is no use for it... just click already in the header -> Consoles...</p>
 
 <?php $this->stop('main_content') ?>
